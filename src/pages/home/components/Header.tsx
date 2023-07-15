dbcdb4dd26f807bac86d7b332b41e19c5527f569
JuanjoCieri <juin@hotmail.com.ar>
2023-07-14 21:57:29 -0300

    image

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -1,21 +1,19 @@
-import desktop3d from "../../../assets/3dIcons/desktop3d.png";
-import fast3d from "../../../assets/3dIcons/fast3d.png";
-import phone3d from "../../../assets/3dIcons/phone3d.png";
+import phoneMockup from "../../../assets/Home/PhoneMockupCropped.png"
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-center">
-      <div>
-        <h1 className="font-extrabold relative max-[700px]:text-7xl inline-block tracking-tighter text-transparent text-8xl bg-clip-text bg-gradient-to-b dark:from-[#585858] dark:to-[#2e2e2e] from-[#313131] to-[#000000]">
+    <header className="w-full flex items-center justify-center">
+      <div className="w-1/2 flex flex-col items-start justify-center">
+        <h1 className="font-extrabold relative inline-block tracking-tighter text-transparent text-7xl bg-clip-text bg-gradient-to-b dark:from-[#585858] dark:to-[#2e2e2e] from-[#313131] to-[#000000]">
           Generador de Mockups
         </h1>
-        <h3 className="font-semibold max-[700px]:text-4xl inline-block tracking-tighter text-3xl text-gray-700">Potenciá tus proyectos con presentaciones impresionantes.</h3>
+        <h3 className="font-semibold inline-block tracking-tighter text-2xl text-gray-700">
+          Potenciá tus proyectos con presentaciones impresionantes.
+        </h3>
       </div>
-      <div className="flex">
-        <img src={desktop3d} alt="" className="w-52" />
-        <img src={fast3d} alt="" className="w-52" />
-        <img src={phone3d} alt="" className="w-52" />
+      <div className="w-1/2 flex justify-end ">
+        <img src={phoneMockup} className="w-96"/>
       </div>
-    </header>
+    </header> 
   );
 }
